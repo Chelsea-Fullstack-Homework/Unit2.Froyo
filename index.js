8337e3f060491bff0e55e302ee6abb43cf19da43
Chelsea Scott <chelseacat62@gmail.com>
2023-10-17 15:31:45 -0700

    add froyo constructor function. created array of strings from input.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+function froyo (flavor, amount) {
+    this.flavor = flavor;
+    this.amount = amount;
+}
+
 let froyoString = prompt("Enter a list of comma-separated froyo flavors", "");
+
+let froyoStringArray = froyoString.split(",");
