14beeb494c17d7c31932b5bb01ba60eee2d44600
Chelsea Scott <chelseacat62@gmail.com>
2023-10-18 20:11:02 -0700

    implement addToCart function. add function call and log statement.

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
-function froyo (flavor, amount) {
+function Froyo (flavor, amount) {
     this.flavor = flavor;
     this.amount = amount;
 }
 
 let froyoString = prompt("Enter a list of comma-separated froyo flavors", "");
 
-let froyoStringArray = froyoString.split(",");
+let arr = froyoString.split(",");
+
+let addToCart = (froyoStringArray) => {
+    if (froyoStringArray[0] == "") return "Cart Empty!";
+
+    let cart = [];
+    let first = true;
+
+    for (let i of froyoStringArray) {
+        if (!first) {
+            for (let j = 0; j < cart.length; j++) {
+                if (i == cart[j].flavor) {
+                    cart[j].amount++;
+                    break;
+                } else if (j == cart.length-1) {
+                    cart.push(new Froyo(i, 1));
+                    break;
+                }
+            }    
+        } else {
+            cart.push(new Froyo(froyoStringArray[0],1));
+            first = false;
+        }
+    }
+    return cart;
+}
+
+
+console.log(addToCart(arr));
